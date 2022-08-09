f4d1978332c62715f21b64f15358298320292490
Paul Jung <pbnjcub@gmail.com>
2022-08-09 19:28:27 -0400

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+// 1.  Create `class`es `Cat`, `Dog`, and `Bird`
+// 2.  Each of these `class`es will accept the _parameters_ `name` and `sex` and
+//     will store those values as _properties_.
+// For each `class`, create the method `speak`.
+
+// - For an _instance_ of `Cat`, speak returns "`name` says meow!",
+// - For an _instance_ of `Dog`, speak returns "`name` says woof!"
+// - For an _instance_ of `Bird`, speak returns conditional output. If the
+
+
+class Cat {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+    
+    speak() {
+        return `${this.name} says meow!`
+    }
+}
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+//   _instance_ of `Bird` is `male`, speak returns "It's me! `name`, the parrot!". If
+//   it is not `male`, speak returns "`name` says squawk!".
+class Bird {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+    speak() {
+        if(this.sex === 'male') {
+            return `It\'s me! ${this.name}, the parrot!`
+        } else{
+            return `${this.name} says squawk!`
+        }
+    }
+}
